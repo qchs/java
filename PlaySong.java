77830e1100a7eb28cd788e595961a84126525ea2
qchs <vera_ziyu@163.com>
2016-06-08 18:38:31 +0800

    A very short code to play a song
    Still a lot to improve

---
+++ PlaySong.java
@@ -0,0 +1,31 @@
+package sound;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
+public class PlaySong {
+
+	public static void main(String[] args) {
+		FileInputStream fis;
+		try {
+			fis = new FileInputStream("miss.wav");
+			AudioStream as = new AudioStream(fis);
+
+			AudioPlayer.player.start(as);
+			
+//			fis.close();  There will be no sound if the file be closed.
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
